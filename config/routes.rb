88c389374c7a3df251242d2cf2d48ad84d0d1459
“cyoung-sudo” <cyoungcs20@gmail.com>
2020-04-26 19:50:37 -0700

    added profiles for other users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
   devise_for :users
+  resources :users, only: [:show]
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_friends', to: 'users#my_friends'
